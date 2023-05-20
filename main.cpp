7657fb9edfeee977463e5b9a47c8db6307b81e29
SegFL <sfernandezl@fi.uba.ar>
2023-05-20 19:53:27 +0000

    Primer commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "mbed.h"
 
+#include "main_system.h"
+
 // main() runs in its own thread in the OS
 int main()
 {
+
+    mainSystemInit();
     while (true) {
 
+        mainSystemUpdate();
     }
 }
 
---
+++ modules/main_system/main_system.h
@@ -0,0 +1,17 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _MAIN_SYSTEM_H_
+#define _MAIN_SYSTEM_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void mainSystemInit();
+void mainSystemUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif 
